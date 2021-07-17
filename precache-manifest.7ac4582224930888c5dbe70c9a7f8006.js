3cae67328ded3420cbc0b844793732828be2c6b5
Hugo Chea <hugochea.pro@outlook.com>
2021-07-17 12:13:52 +0200

    Updates

---
+++ precache-manifest.7ac4582224930888c5dbe70c9a7f8006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77eaadb8e56112e6ab88fa77f3a80810",
+    "url": "/searchJobToolFront/index.html"
+  },
+  {
+    "revision": "c114324af8ba17aa90b7",
+    "url": "/searchJobToolFront/static/css/main.6692bbb8.chunk.css"
+  },
+  {
+    "revision": "c3b4c9ee2c43eb78c630",
+    "url": "/searchJobToolFront/static/js/2.550185a1.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/searchJobToolFront/static/js/2.550185a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c114324af8ba17aa90b7",
+    "url": "/searchJobToolFront/static/js/main.62004082.chunk.js"
+  },
+  {
+    "revision": "702c21c07393859ab6ad",
+    "url": "/searchJobToolFront/static/js/runtime-main.05914415.js"
+  }
+]);
